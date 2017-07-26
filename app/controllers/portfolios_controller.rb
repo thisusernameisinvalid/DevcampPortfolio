793cc5452074c0f7d80cde9f052fd29887db0e40
Jordan Hudgens <jordan@devcamp.com>
2017-07-26 15:28:56 +0000

    Refacored before action for portfolio setting

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,4 +1,5 @@
 class PortfoliosController < ApplicationController
+  before_action :set_portfolio_item, only: [:edit, :update, :show, :destroy]
   layout 'portfolio'
   
   def index
@@ -27,8 +28,6 @@ class PortfoliosController < ApplicationController
   end
 
   def edit
-    @portfolio_item = Portfolio.find(params[:id])
-
   end
 
   def update
@@ -44,13 +43,9 @@ class PortfoliosController < ApplicationController
   end
 
   def show
-    @portfolio_item = Portfolio.find(params[:id])
   end
 
   def destroy
-    # Perform the lookup
-    @portfolio_item = Portfolio.find(params[:id])
-
     # Destroy/delete the record
     @portfolio_item.destroy
 
@@ -70,4 +65,9 @@ class PortfoliosController < ApplicationController
                                      )
   end
 
+
+  def set_portfolio_item
+    @portfolio_item = Portfolio.find(params[:id])
+  
+  end
 end
