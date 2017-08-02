dc61f14285dc751bc4506e2307b27c11e1bec23c
Jordan Hudgens <jordan@devcamp.com>
2017-08-02 01:34:03 +0000

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,23 @@
 module ApplicationHelper
-  def login_helper
-      if current_user.is_a?(GuestUser) 
-       (link_to "Register", new_user_registration_path ) +
-       "<br>".html_safe +
-       (link_to "Login", new_user_session_path) 
-      else
-        link_to "Logout", destroy_user_session_path, method: :delete 
-      end
+  def login_helper style
+    if current_user.is_a?(GuestUser)
+      (link_to "Register", new_user_registration_path, class: style) +
+      " ".html_safe +
+      (link_to "Login", new_user_session_path, class: style)
+    else
+      link_to "Logout", destroy_user_session_path, method: :delete, class: style
+    end
   end
-  
+
   def source_helper(layout_name)
-     if session[:source]
-       greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
-       content_tag(:p, greeting, class: "source-greeting")
-     end 
+    if session[:source]
+      greeting = "Thanks for visiting me from #{session[:source]} and you are on the #{layout_name} layout"
+      content_tag(:p, greeting, class: "source-greeting")
+    end
   end
 
 
   def copyright
-    InvalidViewTool::Renderer.copyright 'thisusernameisinvalid', 'theres a snake in my boot'
+    InvalidViewTool::Renderer.copyright 'Papa MemeLord', 'theres a snake in my boot'
   end
 end
